d8200240549bd75bf2f5146dbdb98111e54a0798
Thibaut Masset Stiegler <thibaut.masset@outlook.fr>
2024-03-05 18:30:07 +0100

    Add taxonomies' labels and admin column

--- Taxonomy/ResourceCategoryTaxonomy.php
+++ Taxonomy/ResourceCategoryTaxonomy.php
@@ -13,7 +13,33 @@ class ResourceCategoryTaxonomy extends BaseTaxonomy
 
     protected const ARGS = array(
         'label' => 'Catégorie',
+        'labels' => array(
+            'name' => 'Catégories',
+            'singular_name' => 'Catégorie',
+            'search_items' => 'Rechercher des catégories',
+            'popular_items' => 'Catégories populaires',
+            'all_items' => 'Toutes les catégories',
+            'edit_item' => 'Modifier la catégorie',
+            'view_item' => 'Voir la catégorie',
+            'update_item' => 'Modifier la catégorie',
+            'add_new_item' => 'Ajouter une catégorie',
+            'new_item_name' => 'Nom de la nouvelle catégorie',
+            'separate_items_with_commas' => 'Séparez les catégories par des virgules',
+            'add_or_remove_items' => 'Ajouter ou retirer des catégories',
+            'choose_from_most_used' => 'Choisissez parmi les catégories les plus utilisées',
+            'not_found' => 'Aucune catégorie trouvée.',
+            'no_terms' => 'Aucune catégorie',
+            'filter_by_item' => 'Filtrer par catégorie',
+            'items_list_navigation' => 'Navigation de la liste des catégories',
+            'items_list' => 'Liste des catégories',
+            'most_used' => 'Plus utilisés',
+            'back_to_items' => '← Aller aux catégories',
+            'item_link' => 'Lien de catégorie',
+            'item_link_description' => 'Un lien vers une catégorie.',
+            'archives' => 'Toutes les catégories',
+         ),
         'public' => true,
+        'show_admin_column' => true,
         'show_in_rest' => true,
     );
 }
