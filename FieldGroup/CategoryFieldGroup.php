ab0d573710a794f8b5989925691fe568b6f99d98
Thibaut Masset Stiegler <thibaut.masset@outlook.fr>
2024-03-09 18:13:03 +0100

    Show category icon in REST API

--- FieldGroup/CategoryFieldGroup.php
+++ FieldGroup/CategoryFieldGroup.php
@@ -41,7 +41,7 @@ class CategoryFieldGroup extends BaseFieldGroup
         'hide_on_screen' => '',
         'active' => true,
         'description' => '',
-        'show_in_rest' => 0,
+        'show_in_rest' => 1,
     );
 
     protected function getLocation()
